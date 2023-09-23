789f870b62a598508a4daed8b50d1409ecd38eef
Rupesh Mangalam <rupeshm.cse21@gmail.com>
2023-09-23 16:25:48 +0000

    db error fix

--- indrabackend/db.js
+++ indrabackend/db.js
@@ -6,13 +6,22 @@ const uri = process.env.MONGODB_URI; // Use an environment variable for the URI
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 async function connectToDatabase() {
-  try {
-    await client.connect();
-    console.log('Connected to the database');
-    return client.db(); // Return the database instance
-  } catch (error) {
-    console.error('Error connecting to the database:', error);
-    throw error;
+  let retries = 3; // Number of retries
+  while (retries > 0) {
+    try {
+      await client.connect();
+      console.log('Connected to the database');
+      return client.db(); // Return the database instance
+    } catch (error) {
+      console.error('Error connecting to the database:', error);
+      retries--; // Decrement the number of retries
+      if (retries === 0) {
+        throw error; // Throw the error if no more retries are left
+      } else {
+        console.log(`Retrying connection... (${retries} retries left)`);
+        await new Promise((resolve) => setTimeout(resolve, 5000)); // Wait for 5 seconds before retrying
+      }
+    }
   }
 }
 
