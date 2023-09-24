1f806625b4d861b92d4398e31459faa193cc6918
Prince <princegujjar1.pp@gmail.com>
2023-09-24 21:17:36 +0530

    URI Changed

--- indrabackend/db.js
+++ indrabackend/db.js
@@ -3,10 +3,10 @@ const  dotenv = require("dotenv");
 dotenv.config();
 
  const connectdb = ()=>{
-    const  DB_USER = process.env.DB_USERNAME;
-    const  DB_PASS = process.env.DB_PASSWORD;
-    const MONGO_URI=`mongodb+srv://${DB_USER}:${DB_PASS}@mern.q0vi3og.mongodb.net/?retryWrites=true&w=majority`
-    mongoose.connect(MONGO_URI);
+    
+  
+  
+    mongoose.connect(process.env.MONGODB_URI);
     
     mongoose.connection.on("connected",()=>{
         console.log("Mongoose is connected");
